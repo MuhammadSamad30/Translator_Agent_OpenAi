b296261e6f482ebae5318d59374bb327798ae64b
MuhammadSamad30 <romivlogs11@gmail.com>
2025-06-03 15:46:04 +0500

    Translator-Agent-openai

--- agent.py
+++ agent.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import asyncio
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+from dotenv import load_dotenv
+import os
 
-
-gemini_api_key = st.secrets.get("GEMINI_API_KEY")
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set in .env file.")
